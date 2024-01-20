76ea36cd38eb8380ed8467abfba468f616571460
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-01-20 17:13:22 -0500

    Added React and Cors for the back end

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose")
 const app = express();
 const PORT = 3000;
+const cors = require("cors");
 const MONGODBURI = "mongodb+srv://joelnwamba:VEDZYkIoXlXzM1x3@book-store-mern.1a5xcxk.mongodb.net/books-collection?retryWrites=true&w=majority"
 
 // const { PORT,  mongoDBURL} = require("./config.js")
@@ -11,6 +12,13 @@ const booksRoute = require("./routes/booksRoute");
 // middleware
 app.use(express.json())
 app.use("/books", booksRoute);
+app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:8000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
 
 app.get("/", (request, response) => {
   console.log(request);
