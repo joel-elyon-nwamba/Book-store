ed0c16d654febd99944d203137f37a1a540ea861
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-01-23 08:29:55 -0500

    Added Axios, and more added front stuff for React

--- front-end-bookStore/src/Pages/Home.jsx
+++ front-end-bookStore/src/Pages/Home.jsx
@@ -1,5 +1,9 @@
-
-
+import React, { useEffect, useState} from "react";
+import axios from "axios";
+import Spinner from "../Compoenent/Spinner";
+import { link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icon/ai";
+import { BsInfoCircle} from "react-icon/bs";
 
 function Home() {
   return(
--- index.js
+++ index.js
@@ -14,11 +14,11 @@ app.use(express.json())
 app.use("/books", booksRoute);
 app.use(cors());
 
-app.use(cors({
-  origin: "http://localhost:8000",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"],
-}));
+// app.use(cors({
+//   origin: "http://localhost:8000",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   allowedHeaders: ["Content-Type"],
+// }));
 
 app.get("/", (request, response) => {
   console.log(request);
