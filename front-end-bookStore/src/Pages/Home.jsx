de3b33247a56a2cbf7d956b2924f419f36d8bee4
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-01-23 20:44:34 -0500

    Added useState to load and useEffect to fetch api

--- front-end-bookStore/src/Pages/Home.jsx
+++ front-end-bookStore/src/Pages/Home.jsx
@@ -1,13 +1,32 @@
 import React, { useEffect, useState} from "react";
 import axios from "axios";
 import Spinner from "../Compoenent/Spinner";
-import { link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icon/ai";
-import { BsInfoCircle} from "react-icon/bs";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle} from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
 
 function Home() {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios.get("http://localhost:3000/books").then((response) => {
+      setBooks(response.data.data);
+      setLoading(false);
+    }).catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
+  }, []);
   return(
-    <div>
+    <div className = "p-4">
+      <div className = "text-3xl my-8">
+        <h1 className="text-3xl my-8">Books List</h1>
+        <Link to="/books/create">
+
+        </Link>
+      </div>
       Home
     </div>
   )
