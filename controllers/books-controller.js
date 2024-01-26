9d3d4f40f590e8421f778976afbaedd862b99565
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-01-25 22:18:23 -0500

    started all over again added the controller route with the delete, update, and edit option for books

--- controllers/books-controller.js
+++ controllers/books-controller.js
@@ -13,7 +13,85 @@ const getAllBooks = async (req, res, next) => {
   if(!books) {
     return res.status(404).json({message: "No product found"});
   }
-  return res.status(200).json({books: books})
+  return res.status(201).json({books: books})
 }
 
-exports.getAllBooks = getAllBooks
+const addBooks = async(req, res, next) => {
+  const {name, author, description, price, available} = req.body;
+  let book;
+  try {
+      book = new Book({
+        name,
+        author,
+        description,
+        price,
+        available
+      });
+      await book.save();
+  }catch(error){
+    console.log(error);
+  }
+  if(!book) {
+    return res.status(500).json({message: "Unable to add product"})
+  }
+  return res.status(201).json({ book })
+}
+
+const getById = async(req, res, next) => {
+  const idInfo = req.params.id;
+  let book;
+
+ try {
+  book = await Book.findById(id);
+ } catch(error) {
+  console.log(error);
+ }
+ if(!book) {
+  return res.status(400).json({message: "No product found"});
+ }
+ return res.status(201).json({ book });
+}
+
+
+const updatedBook = async(req, res, next) => {
+  const id = req.param.id;
+  const {name, author, description, price, available} = req.body;
+  let book;
+  try {
+    book = await Book.findByIdAndUpdate(id, {
+      name,
+      author,
+      description,
+      price,
+      available
+    })
+    book = await book.save();
+
+  } catch(error) {
+    console.log(error);
+  }
+  if(!book) {
+    return res.status(404).json({message: "unable to update"});
+   }
+   return res.status(200).json({ book });
+}
+
+const deleteBook = async(req,res, next) => {
+  const id = req.params.id;
+  let book;
+  try {
+      book = await Book.findByIdAndRemove(id);
+  } catch(error) {
+    console.log(error);
+  }
+  if(!book) {
+    return res.status(404).json({message: "unable to update"});
+   }
+   return res.status(200).json({ book });
+}
+
+exports.getAllBooks = getAllBooks;
+exports.addBooks = addBooks;
+exports.getById = getById;
+exports.updatedBook = updatedBook;
+exports.deleteBook = deleteBook;
