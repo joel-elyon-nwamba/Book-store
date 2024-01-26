c5a50899a38c6437ad363873a7a4135e27b8f97c
joel-nwamba <joel.elyon.nwamba@gmail.com>
2024-01-25 19:28:17 -0500

    Added delete option

--- front-end-bookStore/src/App.jsx
+++ front-end-bookStore/src/App.jsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from "react-router-dom";
 import Home from "./Pages/Home";
 import CreateBooks from "./Pages/CreateBooks";
-import DeleteBook from "./Pages/DeleteBooks";
+import DeleteBooks from "./Pages/DeleteBooks";
 import EditBook from "./Pages/EditBook";
 import ShowBooks from "./Pages/ShowBooks";
 
@@ -15,7 +15,7 @@ function App() {
       <Route path = "/books/create" element= {<CreateBooks />}/>
       <Route path = "/books/details/:id" element= {<ShowBooks />}/>
       <Route path = "/books/edit/:id" element= {<EditBook />}/>
-      <Route path = "/books/delete/:id" element= { <DeleteBook />}/>
+      <Route path = "/books/delete/:id" element= { <DeleteBooks />}/>
     </Routes>
   )
 }
